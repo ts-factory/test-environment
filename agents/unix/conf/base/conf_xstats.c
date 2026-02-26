ts-factory/test-environment

commit aa83b508bc1f73512d138be9c204744c853fc7de

agents/unix: add subtree with ethtool statistics
Add /agent/interface/xstats/ subtree which contains statistics from
'ethtool -S'.

OL-Redmine-Id: 14755
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

agents/unix/conf/base/conf_xstats.c
@@ -1,0 +1,99 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2026 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Unix Test Agent
+ *
+ * Extra Ethernet interface statistics
+ */
+
+#define TE_LGR_USER     "Extra eth xstats Conf"
+
+#include "te_config.h"
+#include "config.h"
+
+#include "logger_api.h"
+#include "unix_internal.h"
+
+#ifdef HAVE_LINUX_ETHTOOL_H
+#include "te_ethtool.h"
+#endif
+
+#include "te_printf.h"
+#include "conf_ethtool.h"
+
+#ifdef HAVE_LINUX_ETHTOOL_H
+te_errno
+xstat_get(unsigned int gid, const char *oid, char *value, const char *if_name,
+          const char *xstats_name, const char *xstat_name)
+{
+    const ta_ethtool_strings *xstat_names = NULL;
+    const uint64_t *xstat_values = NULL;
+    int i;
+    te_errno rc;
+
+    UNUSED(xstats_name);
+
+    rc = ta_ethtool_get_strings_stats(gid, if_name, &xstat_names,
+                                      &xstat_values);
+    if (rc != 0)
+        return rc;
+
+    for (i = 0; i < xstat_names->num; i++)
+    {
+        if (strcmp(xstat_names->strings[i], xstat_name) == 0)
+        {
+            rc = te_snprintf(value, RCF_MAX_VAL, "%" TE_PRINTF_64 "u",
+                             xstat_values[i]);
+            if (rc != 0)
+            {
+                ERROR("%s(): te_snprintf() failed", __FUNCTION__);
+                return TE_RC(TE_TA_UNIX, rc);
+            }
+            return 0;
+        }
+    }
+    return TE_RC(TE_TA_UNIX, TE_ENOENT);
+}
+
+te_errno
+xstat_list(unsigned int gid, const char *oid, const char *sub_id,
+           char **list, const char *if_name)
+{
+    te_errno rc;
+
+    UNUSED(sub_id);
+    UNUSED(oid);
+
+    rc = ta_ethtool_get_strings_list(gid, if_name, ETH_SS_STATS, list);
+    return rc;
+}
+
+static rcf_pch_cfg_object node_xstat = {
+    .sub_id = "xstat",
+    .get = (rcf_ch_cfg_get)xstat_get,
+    .list = (rcf_ch_cfg_list)xstat_list,
+};
+
+static rcf_pch_cfg_object node_xstats = {
+    .sub_id = "xstats",
+    .son = &node_xstat,
+};
+
+/**
+ * Add a child nodes for ethtool statistics to the statistic interface object.
+ *
+ * @return Status code.
+ */
+te_errno
+ta_unix_conf_eth_xstats_init(void)
+{
+    return rcf_pch_add_node("/agent/interface", &node_xstats);
+}
+#else
+te_errno
+ta_unix_conf_eth_xstats_init(void)
+{
+    INFO("Extra ethernet interface statistics are not supported");
+    return 0;
+}
+#endif /* !HAVE_LINUX_ETHTOOL_H */

agents/unix/conf/base/conf.c
@@ -273,6 +273,7 @@ extern te_errno ta_unix_conf_udp_tunnel_init(void);
 extern te_errno ta_unix_conf_bridge_init(void);
 extern te_errno ta_unix_conf_block_dev_init(void);
 extern te_errno ta_unix_conf_l4_port_init(void);
+extern te_errno ta_unix_conf_eth_xstats_init(void);
 
 extern te_errno ta_unix_conf_key_init(void);
 extern void ta_unix_conf_key_fini(void);
@@ -1458,6 +1459,9 @@ rcf_ch_conf_init(void)
             goto fail;
 
         if (ta_unix_conf_eth_init() != 0)
+            goto fail;
+
+        if (ta_unix_conf_eth_xstats_init() != 0)
             goto fail;
 
         if (ta_unix_conf_iommu_init() != 0)

agents/unix/conf/base/conf_ethtool.c
@@ -1002,6 +1002,76 @@ ta_ethtool_get_string_idx(unsigned int gid, const char *if_name,
     return TE_RC(TE_TA_UNIX, TE_ENOENT);
 }
 
+/* see description in conf_ethtool.h */
+te_errno
+ta_ethtool_get_strings_stats(unsigned int gid, const char *if_name,
+                             const ta_ethtool_strings **stat_names,
+                             const uint64_t **stat_values)
+{
+    struct ethtool_stats *stats = NULL;
+    te_string obj_name = TE_STRING_INIT_STATIC(MAX_OBJ_NAME_LEN);
+    ta_cfg_obj_t *obj;
+    uint64_t *result;
+    size_t req_size;
+    te_errno rc;
+    size_t stats_num;
+
+    rc = ta_ethtool_get_strings(gid, if_name, ETH_SS_STATS, stat_names);
+    if (rc != 0)
+        return rc;
+    stats_num = (*stat_names)->num;
+
+    te_string_append(&obj_name, "%s.%u", if_name, ETHTOOL_GSTATS);
+
+    obj = ta_obj_find(TA_OBJ_TYPE_IF_XSTATS, te_string_value(&obj_name),
+                      gid);
+    if (obj != NULL)
+    {
+        *stat_values = obj->user_data;
+        return 0;
+    }
+
+    req_size = sizeof(struct ethtool_stats) +
+                            sizeof(uint64_t) * stats_num;
+
+    stats = TE_ALLOC(req_size);
+
+    stats->cmd = ETHTOOL_GSTATS;
+    stats->n_stats = stats_num;
+
+    rc = call_ethtool_ioctl(if_name, ETHTOOL_GSTATS, stats);
+    if (rc != 0)
+    {
+        free(stats);
+        return rc;
+    }
+    if (stats->n_stats != stats_num)
+    {
+        ERROR("%s(): amount of stats is different for "
+              "ETHTOOL_GSTATS(%d) and ETH_SS_STATS(%zu)",
+              __FUNCTION__, stats->n_stats, stats_num);
+        free(stats);
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+    }
+
+    req_size = sizeof(uint64_t) * stats_num;
+    result = TE_ALLOC(req_size);
+
+    memcpy(result, stats->data, stats_num * sizeof(uint64_t));
+    free(stats);
+
+    rc = ta_obj_add(TA_OBJ_TYPE_IF_XSTATS, te_string_value(&obj_name),
+                    "", gid, result, &free, &obj);
+    if (rc != 0)
+    {
+        free(result);
+        return rc;
+    }
+
+    *stat_values = result;
+    return rc;
+}
+
 #ifdef ETHTOOL_GRSSH
 
 static te_errno

agents/unix/conf/base/conf_ethtool.h
@@ -447,6 +447,23 @@ extern te_errno ta_ethtool_get_string_idx(unsigned int gid,
                                           const char *target,
                                           unsigned int *idx);
 
+/**
+ * Get set of statistics names and values.
+ *
+ * @param gid         Request group ID
+ * @param if_name     Interface name
+ * @param stat_names  Where to save pointer to filled ta_ethtool_strings
+ *                    with statistics names (caller should not release it).
+ * @param stat_values Where to save pointer to filled array of statistics
+ *                    values (caller should not release it)
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_get_strings_stats(unsigned int gid,
+                                       const char *if_name,
+                                       const ta_ethtool_strings **stat_names,
+                                       const uint64_t **stat_values);
+
 #ifdef ETHTOOL_GRSSH
 
 /** Data associated with ETHTOOL_GRSSH/ETHTOOL_SRSSH */

agents/unix/conf/meson.build
@@ -45,6 +45,7 @@ sources += files(
     'base/conf_veth.c',
     'base/conf_udp_tunnel.c',
     'base/conf_eee.c',
+    'base/conf_xstats.c',
     'process/conf_process.c',
     'route/conf_route.c',
     'route/conf_route_libnetconf.c',

doc/cm/cm_base.yml
@@ -1053,6 +1053,23 @@
          Name: none
          Value: any value means that reset is requested
 
+    - oid: "/agent/interface/xstats"
+      access: read_only
+      type: none
+      d: |
+         Network interface extended statistics
+         Name: none
+         Value: none
+
+    - oid: "/agent/interface/xstats/xstat"
+      access: read_only
+      type: uint64
+      volatile: true
+      d: |
+         Network interface certain extended statistic
+         Name:  Name of statistic
+         Value: Value of statistic
+
     - oid: "/agent/loadavg"
       access: read_only
       type: none

lib/rcfpch/rcf_pch_ta_cfg.h
@@ -317,6 +317,12 @@ extern void ta_cfg_obj_log(const ta_cfg_obj_t *obj);
 #define TA_OBJ_TYPE_IF_STRINGS "if_strings"
 
 /**
+ * Name for the object type containing set of statistic values related
+ * to network interface.
+ */
+#define TA_OBJ_TYPE_IF_XSTATS "if_xstats"
+
+/**
  * Name for the object type containing RSS hash key and indirection
  * table.
  */

commit dc2c2f439320f3078805f8e09ed06e471acc69a4

agent/unix: do not fail if statistics is not supported
xstat_list() should return NULL list without error in case when
interface doesn't support statistics.

OL-Redmine-Id: 14755
Fixes: aa83b508bc1f ("agents/unix: add subtree with ethtool statistics")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

agents/unix/conf/base/conf_xstats.c
@@ -65,6 +65,35 @@ xstat_list(unsigned int gid, const char *oid, const char *sub_id,
     UNUSED(oid);
 
     rc = ta_ethtool_get_strings_list(gid, if_name, ETH_SS_STATS, list);
+    if (rc != 0)
+    {
+        int failed_ethtool_cmd;
+
+        failed_ethtool_cmd = ta_ethtool_failed_cmd();
+
+        if (failed_ethtool_cmd < 0)
+        {
+            ERROR("%s(): error %r occurred while getting statistics for %s",
+                  __FUNCTION__, rc, if_name);
+        }
+        else if (rc != TE_RC(TE_TA_UNIX, TE_EOPNOTSUPP) ||
+                 failed_ethtool_cmd != ETHTOOL_GSTRINGS)
+        {
+            ERROR("%s(): error %r occurred while getting statistics for %s; "
+                  "failed SIOCETHTOOL command is %d (%s)", __FUNCTION__, rc,
+                  if_name, failed_ethtool_cmd,
+                  ta_ethtool_cmd2str(failed_ethtool_cmd));
+        }
+        else
+        {
+            /*
+             * If statistics are not supported, let Configurator
+             * think they are not present to avoid error messages.
+             */
+            *list = NULL;
+            rc = 0;
+        }
+    }
     return rc;
 }
 