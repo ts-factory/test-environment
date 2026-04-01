ts-factory/test-environment

commit d718dc03f7955d1aac30c530a98a9d19a2b8cebe

agent/unix: ignore IRQs which are not in /proc/interrupts
Do not fail if IRQ in /sys/class/net/<ifname>/device/msi_irqs and in
/proc/irq/ but not present in /proc/interrupts, add INFO() and go
forth.

OL-Redmine-Id: 14768
Fixes: 89a6ecf91cc7 ("agent/unix: add CS subtree for interface IRQs")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

agents/unix/conf/base/conf_irq.c
@@ -129,6 +129,20 @@ add_irq_obj(te_vec *irqs_vec, unsigned int irq_num)
     TE_VEC_APPEND(irqs_vec, irq_obj);
 }
 
+static te_errno
+del_irq_obj(te_vec *irqs_vec, unsigned int irq_num)
+{
+    unsigned int index = 0;
+    bool found = false;
+
+    found = te_vec_search(irqs_vec, &irq_num, &search_irqs, &index, NULL);
+    if (!found)
+        return TE_RC(TE_TA_UNIX, TE_ENOENT);
+
+    te_vec_remove_index(irqs_vec, index);
+    return 0;
+}
+
 static unsigned int
 get_legacy_irq(const char *if_name)
 {
@@ -349,10 +363,18 @@ ta_irq_fill_objs(unsigned int gid, const char *if_name)
             {
                 if (rewind_is_done)
                 {
-                    ERROR("Failed to find IRQ:%u in /proc/interrupts.",
-                          irq_obj->irq_num);
-                    rc = TE_RC(TE_TA_UNIX, TE_EINVAL);
-                    goto cleanup;
+                    INFO("Failed to find IRQ:%u in /proc/interrupts.",
+                         irq_obj->irq_num);
+                    rc = del_irq_obj(result_irqs, irq_obj->irq_num);
+                    if (rc == 0)
+                    {
+                        rewind(f);
+                        continue;
+                    }
+                    else
+                    {
+                        goto cleanup;
+                    }
                 }
                 else
                 {

commit 8c604740e68a588c1f67c31f53cc8da2a04e7ed7

agent/unix: fix getting of IRQs list
Fix deleting of IRQ which was found in
/sys/class/net/../device/msi_irqs and not found in
/proc/interrupts.

OL-Redmine-Id: 14791
Fixes: d718dc03f795 ("agent/unix: ignore IRQs which are not in /proc/interrupts")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

agents/unix/conf/base/conf_irq.c
@@ -286,6 +286,7 @@ ta_irq_fill_objs(unsigned int gid, const char *if_name)
     te_string obj_name = TE_STRING_INIT_STATIC(CFG_OID_MAX);
     ta_cfg_obj_t *obj;
     te_vec *result_irqs = NULL;
+    te_vec remove_irqs = TE_VEC_INIT(unsigned int);
     char line[1024];
     te_vec  cpus;
     struct ta_irq_obj *irq_obj;
@@ -295,6 +296,7 @@ ta_irq_fill_objs(unsigned int gid, const char *if_name)
     te_errno rc = 0;
     int  rest_len;
     unsigned long val;
+    unsigned int *rem_irq;
 
     cpus = TE_VEC_INIT(struct ta_irq_per_cpu);
 
@@ -356,6 +358,7 @@ ta_irq_fill_objs(unsigned int gid, const char *if_name)
         char *current;
         unsigned int irq_val;
         struct ta_irq_per_cpu *irq_per_cpu;
+        bool removed_irq = false;
 
         while (true)
         {
@@ -365,16 +368,10 @@ ta_irq_fill_objs(unsigned int gid, const char *if_name)
                 {
                     INFO("Failed to find IRQ:%u in /proc/interrupts.",
                          irq_obj->irq_num);
-                    rc = del_irq_obj(result_irqs, irq_obj->irq_num);
-                    if (rc == 0)
-                    {
-                        rewind(f);
-                        continue;
-                    }
-                    else
-                    {
-                        goto cleanup;
-                    }
+                    TE_VEC_APPEND(&remove_irqs, irq_obj->irq_num);
+                    rewind(f);
+                    removed_irq = true;
+                    break;
                 }
                 else
                 {
@@ -387,6 +384,8 @@ ta_irq_fill_objs(unsigned int gid, const char *if_name)
             if (irq_val == irq_obj->irq_num)
                 break;
         }
+        if (removed_irq)
+            continue;
         current = strchr(line, ':') + 1;
 
         TE_VEC_FOREACH(&cpus, irq_per_cpu)
@@ -408,6 +407,12 @@ ta_irq_fill_objs(unsigned int gid, const char *if_name)
         if (rc != 0)
             goto cleanup;
     }
+    TE_VEC_FOREACH(&remove_irqs, rem_irq)
+    {
+        rc = del_irq_obj(result_irqs, *rem_irq);
+        if (rc != 0)
+            goto cleanup;
+    }
 
     rc = ta_obj_add(TA_OBJ_TYPE_IF_IRQ,
                     te_string_value(&obj_name),
@@ -419,6 +424,7 @@ cleanup:
     if (f != NULL)
         fclose(f);
     te_vec_free(&cpus);
+    te_vec_free(&remove_irqs);
     te_string_free(&obj_name);
 
     return rc;