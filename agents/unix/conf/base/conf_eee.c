c13d0995aa00d37c5488fb90b979a6cc214f0ebc
Konstantin Ushakov <konstantin.ushakov@oktet.tech>
2026-08-28 23:48:44 +0000

    agent/unix: move eee subtree to ta_conf
    Every leaf takes the type its object is declared with in
    cm_base.yml: eee_active and eee_enabled are boolean, tx_lpi_enabled
    is 64-bit unsigned and tx_lpi_timer 32-bit unsigned. The two boolean
    nodes were reading a boolean object through an unsigned accessor;
    their getters now range-check what the shared field reader returns
    before narrowing it to a bool, and the legacy setter
    (common_param_set), which never restricted values to 0/1, keeps its
    explicit val > UINT_MAX check for the two unsigned leaves.
    
    Signed-off-by: Konstantin Ushakov <konstantin.ushakov@oktet.tech>

--- agents/unix/conf/base/conf_eee.c
+++ agents/unix/conf/base/conf_eee.c
@@ -11,12 +11,12 @@
 #include "te_config.h"
 #include "config.h"
 
+#include <limits.h>
+
 #include "te_errno.h"
 #include "logger_api.h"
 #include "te_defs.h"
-#include "rcf_pch.h"
-#include "rcf_pch_ta_cfg.h"
-#include "unix_internal.h"
+#include "rcf_pch_tree.h"
 
 #ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
@@ -67,19 +67,14 @@ get_field_ptr(struct ethtool_eee *eptr, eee_field field)
 
 /* Common code for getting field value */
 static te_errno
-common_field_get(unsigned int gid,
-                 const char *oid,
-                 char *value,
-                 const char *if_name,
-                 eee_field field)
+common_field_get(ta_conf_ctx *ctx, uintmax_t *val, eee_field field)
 {
+    const char *if_name = ta_conf_ctx_inst(ctx, "interface");
     struct ethtool_eee *eptr;
     uint32_t *field_ptr;
     te_errno rc;
 
-    UNUSED(oid);
-
-    rc = get_ethtool_value(if_name, gid, TA_ETHTOOL_EEE,
+    rc = get_ethtool_value(if_name, ta_conf_ctx_gid(ctx), TA_ETHTOOL_EEE,
                            (void **)&eptr);
     if (rc != 0)
     {
@@ -98,44 +93,26 @@ common_field_get(unsigned int gid,
     if (field_ptr == NULL)
         return TE_RC(TE_TA_UNIX, TE_ENOENT);
 
-    rc = te_snprintf(value, RCF_MAX_VAL, "%u", (unsigned int)(*field_ptr));
-    if (rc != 0)
-    {
-        ERROR("%s(): te_snprintf() failed, %r", __FUNCTION__, rc);
-        return TE_RC(TE_TA_UNIX, rc);
-    }
-
+    *val = *field_ptr;
     return 0;
 }
 
 /* Common code for setting field value */
 static te_errno
-common_param_set(unsigned int gid,
-                 const char *oid,
-                 const char *value,
-                 const char *if_name,
-                 eee_field field)
+common_param_set(ta_conf_ctx *ctx, uintmax_t val, eee_field field)
 {
+    const char *if_name = ta_conf_ctx_inst(ctx, "interface");
     struct ethtool_eee *eptr;
     uint32_t *field_ptr;
-    unsigned long int parsed_val;
     te_errno rc;
 
-    UNUSED(oid);
-
-    rc = te_strtoul(value, 10, &parsed_val);
-    if (rc != 0)
+    if (val > UINT_MAX)
     {
-        ERROR("%s(): invalid value '%s'", __FUNCTION__, value);
-        return rc;
-    }
-    else if (parsed_val > UINT_MAX)
-    {
-        ERROR("%s(): too big value '%s'", __FUNCTION__, value);
+        ERROR("%s(): too big value '%ju'", __FUNCTION__, val);
         return TE_RC(TE_TA_UNIX, TE_EINVAL);
     }
 
-    rc = get_ethtool_value(if_name, gid, TA_ETHTOOL_EEE,
+    rc = get_ethtool_value(if_name, ta_conf_ctx_gid(ctx), TA_ETHTOOL_EEE,
                            (void **)&eptr);
     if (rc != 0)
         return rc;
@@ -144,97 +121,99 @@ common_param_set(unsigned int gid,
     if (field_ptr == NULL)
         return TE_RC(TE_TA_UNIX, TE_ENOENT);
 
-    *field_ptr = parsed_val;
+    *field_ptr = val;
     return 0;
 }
 
 /* Get eee_active field */
 static te_errno
-eee_active_get(unsigned int gid, const char *oid, char *value,
-               const char *if_name)
+eee_active_get(ta_conf_ctx *ctx, bool *val)
 {
-    return common_field_get(gid, oid, value, if_name, FIELD_EEE_ACTIVE);
+    uintmax_t raw;
+    te_errno rc = common_field_get(ctx, &raw, FIELD_EEE_ACTIVE);
+
+    if (rc != 0)
+        return rc;
+    if (raw > 1)
+        return TE_RC(TE_TA_UNIX, TE_ERANGE);
+    *val = raw != 0;
+    return 0;
 }
 
 /* Get eee_enabled field */
 static te_errno
-eee_enabled_get(unsigned int gid, const char *oid, char *value,
-                const char *if_name)
+eee_enabled_get(ta_conf_ctx *ctx, bool *val)
 {
-    return common_field_get(gid, oid, value, if_name, FIELD_EEE_ENABLED);
+    uintmax_t raw;
+    te_errno rc = common_field_get(ctx, &raw, FIELD_EEE_ENABLED);
+
+    if (rc != 0)
+        return rc;
+    if (raw > 1)
+        return TE_RC(TE_TA_UNIX, TE_ERANGE);
+    *val = raw != 0;
+    return 0;
 }
 
 /* Get tx_lpi_enabled field */
 static te_errno
-tx_lpi_enabled_get(unsigned int gid, const char *oid, char *value,
-                   const char *if_name)
+tx_lpi_enabled_get(ta_conf_ctx *ctx, uint64_t *val)
 {
-    return common_field_get(gid, oid, value, if_name, FIELD_TX_LPI_ENABLED);
+    return common_field_get(ctx, val, FIELD_TX_LPI_ENABLED);
 }
 
 /* Get tx_lpi_timer field */
 static te_errno
-tx_lpi_timer_get(unsigned int gid, const char *oid, char *value,
-                 const char *if_name)
+tx_lpi_timer_get(ta_conf_ctx *ctx, uint32_t *val)
 {
-    return common_field_get(gid, oid, value, if_name, FIELD_TX_LPI_TIMER);
+    uintmax_t raw;
+    te_errno rc = common_field_get(ctx, &raw, FIELD_TX_LPI_TIMER);
+
+    if (rc != 0)
+        return rc;
+    if (raw > UINT32_MAX)
+        return TE_RC(TE_TA_UNIX, TE_ERANGE);
+    *val = raw;
+    return 0;
 }
 
 /* Set eee_enabled field */
 static te_errno
-eee_enabled_set(unsigned int gid, const char *oid, const char *value,
-                const char *if_name)
+eee_enabled_set(ta_conf_ctx *ctx, bool val)
 {
-    return common_param_set(gid, oid, value, if_name, FIELD_EEE_ENABLED);
+    return common_param_set(ctx, val, FIELD_EEE_ENABLED);
 }
 
 /* Set tx_lpi_enabled field */
 static te_errno
-tx_lpi_enabled_set(unsigned int gid, const char *oid, const char *value,
-                   const char *if_name)
+tx_lpi_enabled_set(ta_conf_ctx *ctx, uint64_t val)
 {
-    return common_param_set(gid, oid, value, if_name, FIELD_TX_LPI_ENABLED);
+    return common_param_set(ctx, val, FIELD_TX_LPI_ENABLED);
 }
 
 /* Set tx_lpi_timer field */
 static te_errno
-tx_lpi_timer_set(unsigned int gid, const char *oid, const char *value,
-                 const char *if_name)
+tx_lpi_timer_set(ta_conf_ctx *ctx, uint32_t val)
 {
-    return common_param_set(gid, oid, value, if_name, FIELD_TX_LPI_TIMER);
+    return common_param_set(ctx, val, FIELD_TX_LPI_TIMER);
 }
 
 /* Commit changes to EEE configuration */
 static te_errno
-eee_commit(unsigned int gid, const cfg_oid *p_oid)
+eee_commit(ta_conf_ctx *ctx)
 {
-    char *if_name;
+    const char *if_name = ta_conf_ctx_inst(ctx, "interface");
 
-    UNUSED(gid);
-    if_name = CFG_OID_GET_INST_NAME(p_oid, 2);
-
-    return commit_ethtool_value(if_name, gid, TA_ETHTOOL_EEE);
+    return commit_ethtool_value(if_name, ta_conf_ctx_gid(ctx), TA_ETHTOOL_EEE);
 }
 
-static rcf_pch_cfg_object node_eee;
-
-RCF_PCH_CFG_NODE_RWC(node_tx_lpi_timer, "tx_lpi_timer", NULL,
-                     NULL, tx_lpi_timer_get, tx_lpi_timer_set,
-                     &node_eee);
-
-RCF_PCH_CFG_NODE_RWC(node_tx_lpi_enabled, "tx_lpi_enabled", NULL,
-                     &node_tx_lpi_timer, tx_lpi_enabled_get, tx_lpi_enabled_set,
-                     &node_eee);
-
-RCF_PCH_CFG_NODE_RWC(node_eee_enabled, "eee_enabled", NULL,
-                     &node_tx_lpi_enabled, eee_enabled_get, eee_enabled_set,
-                     &node_eee);
-
-RCF_PCH_CFG_NODE_RO(node_eee_active, "eee_active", NULL,
-                    &node_eee_enabled, eee_active_get);
-
-RCF_PCH_CFG_NODE_NA_COMMIT(node_eee, "eee", &node_eee_active, NULL,
-                           eee_commit);
+static const ta_conf_node *const node_eee =
+    TA_CONF_NA_COMMIT("eee", eee_commit,
+        TA_CONF_RO_BOOL("eee_active", eee_active_get),
+        TA_CONF_RW_BOOL("eee_enabled", eee_enabled_get, eee_enabled_set),
+        TA_CONF_RW_UINT64("tx_lpi_enabled", tx_lpi_enabled_get,
+                        tx_lpi_enabled_set),
+        TA_CONF_RW_UINT32("tx_lpi_timer", tx_lpi_timer_get, tx_lpi_timer_set));
 
 /**
  * Add "eee" node to interface in configuration tree.
@@ -244,7 +223,7 @@ RCF_PCH_CFG_NODE_NA_COMMIT(node_eee, "eee", &node_eee_active, NULL,
 extern te_errno
 ta_unix_conf_if_eee_init(void)
 {
-    return rcf_pch_add_node("/agent/interface", &node_eee);
+    return ta_conf_register("/agent/interface", node_eee);
 }
 
 #else
